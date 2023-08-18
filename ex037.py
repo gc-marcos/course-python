58f229ac157f1c8fa0eebc68c8302122e7ddb6e6
Marcos Carvalho <gcmarcoss@gmail.com>
2023-08-18 19:06:50 -0300

    elif

--- ex037.py
+++
@@ -1,9 +0,0 @@
-casa = float(input('Qual o valor da casa? R$ '))
-salario = float(input('Qual o valor do seu salário? R$ '))
-anos = int(input('Em quantos anos pretende pagar? '))
-prest = anos * 12
-valor_pres = casa / prest
-if valor_pres <= salario * 30 / 100:
-    print('Sua prestação será de R$ {:.2f} e foi Aprovado sua compra.'.format(valor_pres))
-else:
-    print('O valor da sua prestação é de R$ {:.2f} e ultrapassou 30% do valor de seu salario'.format(valor_pres))
--- ex038.py
+++ ex038.py
@@ -1,3 +1,8 @@
-nome = str(input('Digite o nome: '))
-quat = len(nome)
-print('tamanho do nome é : {}'.format(quat))
+num1 = int(input('Digite um numero: '))
+num2 = int(input('Digite outro numero: '))
+if num1 > num2:
+    print('O primeiro numero é maior!')
+if num1 < num2:
+    print('O segundo numero é maior!')
+else:
+    print('Os dois são iguais!!')
